cd25d4f6bbcbecfaf9963fd08babbf62d7106dee
leandrobeandrade <leandrobe.andrade@gmail.com>
2023-06-27 23:46:21 -0300

    Testes Http

---
+++ proj1/src/app/request/request.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from "../services/http/http.service";
+
+@Component({
+  selector: 'app-request',
+  templateUrl: './request.component.html',
+  styleUrls: ['./request.component.css']
+})
+export class RequestComponent implements OnInit {
+  name!: string;
+  email!: string;
+  age!: string;
+
+  constructor(private http: HttpService) { }
+
+  ngOnInit(): void {
+  }
+
+  getUsers() {
+    this.http.getUsers().subscribe()
+  }
+
+  postUsers() {
+    const user = {
+      id: Math.random(),
+      name: this.name,
+      email: this.email,
+      age: this.age
+    }
+
+    this.http.postUser(user).subscribe();
+  }
+
+  putUsers(id: any) {
+    const user = {
+      name: this.name,
+      email: this.email,
+      age: this.age
+    }
+    this.http.putUser(id, user).subscribe();
+  }
+
+  deleteUser(id: number) {
+    return this.http.deleteUser(id).subscribe();
+  }
+
+  getUsersWithHeaders() {
+    this.http.getUserWithHeaders().subscribe();
+  }
+
+  getUsersById(id: number) {
+    this.http.getUsersById(id).subscribe();
+  }
+
+}
