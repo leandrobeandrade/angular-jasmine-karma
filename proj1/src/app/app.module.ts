67306eedce7218b2113d589975b81c29dad30bf7
leandrobeandrade <leandrobe.andrade@gmail.com>
2023-06-29 23:57:48 -0300

    start

--- proj1/src/app/app.module.ts
+++ proj1/src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatDialogModule } from "@angular/material/dialog";
+
 import { AppComponent } from './app.component';
-import { ConceitosIniciaisComponent } from './conceitos-iniciais/conceitos-iniciais.component';
+import { InitConceptsComponent } from './init-concepts/init-concepts.component';
+import { FixProblemsComponent } from './fix-problems/fix-problems.component';
+import { StubComponent } from './stub/stub.component';
+import { DebuggerComponent } from './debugger/debugger.component';
+import { MatchersComponent } from './matchers/matchers.component';
+import { SpyonComponent } from './spyon/spyon.component';
+import { RequestComponent } from './request/request.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConceitosIniciaisComponent
+    InitConceptsComponent,
+    FixProblemsComponent,
+    StubComponent,
+    DebuggerComponent,
+    MatchersComponent,
+    SpyonComponent,
+    RequestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
